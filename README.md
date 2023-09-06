12cf9982902d2a5a46a91d0bc1998e4fbed43b55
lani <laniiiiworld@gmail.com>
2023-09-06 11:32:33 +0900

    Implement search header

--- README.md
+++ README.md
@@ -6,12 +6,13 @@
 ## 기능
 
 - React Router를 사용하여 메인/검색/상세 등의 각 페이지로 이동
+- 유튜브 API를 이용하여 인기있는 동영상 목록 제공 및 관련 카테고리 동영상 추천, 키워드 검색 기능 구현
 
 ## 사용 기술
 
 ### 기술
 
-- Tailwind
+- Tailwind (v3.3.3)
 - JavaScript
 - React
 
@@ -19,6 +20,7 @@
 
 - React Router (v6.15.0)
 - TanStack Query
+- React Icons
 
 ### API
 
---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { FaYoutube, FaGithub } from 'react-icons/fa';
+import { GoSearch } from 'react-icons/go';
+
+export default function SearchHeader() {
+  const { keyword } = useParams();
+  const [text, setText] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+    navigate(`/videos/${text.trim()}`);
+  };
+
+  useEffect(() => setText(keyword || ''), [keyword]);
+
+  return (
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4 justify-between'>
+      <Link to='/' className='flex items-center'>
+        <FaYoutube className='text-4xl text-brand' />
+        <h1 className='font-bold ml-2 text-3xl'>YouTube</h1>
+      </Link>
+      <form onSubmit={handleSubmit} className='w-5/12 flex items-center'>
+        <input //
+          type='text'
+          placeholder='Search...'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className='w-full p-2 pl-5 outline-none bg-black text-gray-500 border border-zinc-900 rounded-s-full'
+        />
+        <button className='bg-zinc-600 hover:bg-zinc-500 px-5 py-3 border border-l-0 border-zinc-900 rounded-e-full'>
+          <GoSearch />
+        </button>
+      </form>
+      <div className='flex items-center'>
+        <button
+          onClick={() => window.open(process.env.REACT_APP_GITHUB_URL)}
+          className='text-4xl rounded-full hover:bg-zinc-700'
+        >
+          <FaGithub />
+        </button>
+      </div>
+    </header>
+  );
+}
