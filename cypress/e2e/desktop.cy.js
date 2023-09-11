74f5efed670347d53f0fd779fe71fd2ff0d33240
lani <laniiiiworld@gmail.com>
2023-09-11 21:47:55 +0900

    Writing e2e tests with cypress

---
+++ cypress/e2e/desktop.cy.js
@@ -0,0 +1,39 @@
+///<reference types='cypress' />
+import '@testing-library/cypress/add-commands';
+
+describe('YouTube App - desktop mode', () => {
+  beforeEach(() => {
+    cy.intercept('GET', /(popular)/g, { fixture: 'popular.json' });
+    cy.intercept('GET', /(search)/g, { fixture: 'search.json' });
+    cy.viewport('macbook-13');
+    cy.visit('/');
+  });
+
+  it('renders', () => {
+    cy.findByTitle('logo').should('exist');
+    cy.findByText('YouTube').should('exist');
+  });
+
+  it('shows popular video first', () => {
+    cy.findByText('Popular Video').should('exist');
+  });
+
+  it('searches by keywords', () => {
+    cy.findByPlaceholderText('Search...').type('bts');
+
+    cy.findByTitle('search').click();
+
+    cy.findByText('Search Video').should('exist');
+  });
+
+  it('goes to the detail page when the video is clicked', () => {
+    cy.findAllByRole('listitem').first().click();
+
+    cy.findByTitle('Popular Video').should('exist');
+    cy.findByText('Popular Video').should('exist');
+    cy.findByAltText('Channel Title');
+    cy.findByText('Channel Title');
+    cy.findByText('This is a description of a popular video.');
+    cy.findByText('Search Video').should('exist');
+  });
+});
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -19,7 +19,7 @@ export default function SearchHeader() {
   return (
     <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4 justify-between'>
       <Link to='/' className='flex items-center'>
-        <FaYoutube className='text-4xl text-brand' />
+        <FaYoutube className='text-4xl text-brand' title='logo' />
         <h1 className='font-bold ml-2 text-3xl'>YouTube</h1>
       </Link>
       <form onSubmit={handleSubmit} className='w-5/12 flex items-center'>
@@ -30,13 +30,17 @@ export default function SearchHeader() {
           onChange={(e) => setText(e.target.value)}
           className='w-full p-2 pl-5 outline-none bg-black text-gray-500 border border-zinc-900 rounded-s-full'
         />
-        <button className='bg-zinc-600 hover:bg-zinc-500 px-5 py-3 border border-l-0 border-zinc-900 rounded-e-full'>
+        <button
+          className='bg-zinc-600 hover:bg-zinc-500 px-5 py-3 border border-l-0 border-zinc-900 rounded-e-full'
+          title='search'
+        >
           <GoSearch />
         </button>
       </form>
       <div className='flex items-center'>
         <button
           onClick={() => window.open(process.env.REACT_APP_GITHUB_URL)}
+          title='github'
           className='text-4xl rounded-full hover:bg-zinc-700'
         >
           <FaGithub />
